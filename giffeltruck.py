0b7d732075846a542432b0a3964aaec2f81f3131
Simon Brenner <olsner@gmail.com>
2024-03-05 11:36:38 +0100

    Add instructions and keymap and inline the resources

--- giffeltruck.py
+++ giffeltruck.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
+################################################################################
+#                                   KEY MAP                                    #
+#                                                                              #
+#   q                       Quit game                                          #
+#   Arrow keys              Turn towards the direction, or if already facing   #
+#                           it drive forwards.                                 #
+#   x                       Drive backwards given the current direction. Use   #
+#                           arrow keys to turn if necessary.                   #
+#   u                       Lift forks, picking up a giffel (@@) if the forks  #
+#                           are in position                                    #
+#   d                       Lower forks, dropping off the loaded giffel        #
+#                                                                              #
+#                                                                              #
+#   OBJECTIVE:                                                                 #
+#                                                                              #
+#   Pick up all giffels (@@) and drive them to some goal position (..). The    #
+#   truck or forks must not touch the walls. Although it doesn't kill you,     #
+#   your boss will be unhappy.                                                 #
+#                                                                              #
+################################################################################
 
 import curses
 from enum import Enum
 import math
 import sys
 import time
+import zlib
 
-# TODO Obfuscate and inline contents.
-WIN_SCREEN = open("win.txt").read().split("\n")
+def decode(x):
+    return zlib.decompress(x).decode('ascii').split('\n')
+
+WIN_SCREEN = decode(b"x\xda\x9dPA\x0e\xc2@\x08\xbc\xf3\n\x8ez)\x1f0\xbe\x84\x84h\xf4\xd6Sco<^\x18Xmb\xbd\x94l\xc2\xec0\x0c\xec\x121[\xc7d\xd6\x97O\xca\x98l\x1b\xbc\x17E\x13K7\x8a\x9b\xb9\x00k'\xf4:\xb3\x966EmX\xa7:S\xe1\x81(\x9cX\xc19@ \xed\x94\x86\xac\x10zZ\xc1\x14;h_0\x12\na\xa1l\x0bR\xe1\xe3\xe8\xe3\x9aS\x03$\xb1\r\xab\xae\x08\x80(\x16\xae\xe2%\x1e\xa8\xe3\x13by\xf7*TR\xd0W\x1b\x0f\xfbV\x1c\xdcF\xe1\xb1)\x8d\xaf\xcbI;\xf1\x87\xfeU\xa8\x10\xf1\xe18=\x9e/\xbe\xaf\xf3|[x]\xce\xf4\x06\xa0$z\xb2")
+MAP_DATA = decode(b'x\xda}Q\xc9\r\xc00\x08\xfb3\x85%\xe7\xdd\x19\xba\xffTm\x028\xa0\x1eH\xad8\x1cc\x00|3,\x9b\x9ea\x06\x9cA\xe4f\xb4q\x05\x00\xc4\xaf\x19\x8d\x1b\xc0\xf4\xf5\xdd)#\xce$U6Z\xd2\x19\x92\xb7\xd4SL\x00"\xf4\x1e\x1a!)\x8d[{\x16T\xa6k\x90tA\xca\x83l\x01\xd4\xc6\x9ai\x04`\x11\xb2\xee\xc1\x9dA\xdf\x03;\x8b\xd8w\x0b-\x92\r\xb7T>\xc7\xd4^]\xea\x9a\xe28\xda\xae\xd8o\x81\x8f+\xe9\x16\xff\x06\xbb\x00\x86\xdfGe')
 COLLISION_PENALTY = 0.5
 COLLISION_ATTR = 0
 DEFAULT_ATTR = 0
@@ -106,9 +130,6 @@ class Player():
             for dx,c in enumerate(s):
                 if c != ' ':
                     scr.addstr(self.y - 1 + dy, 2 * (self.x - 1 + dx), c + c)
-#        scr.addstr(self.y - 1, 2 * (self.x - 1), graphic[0])
-#        scr.addstr(self.y    , 2 * (self.x - 1), graphic[1])
-#        scr.addstr(self.y + 1, 2 * (self.x - 1), graphic[2])
 
     def would_collide_at(self, x, y):
         return self.world.has_wall_at(x, y)
@@ -153,7 +174,7 @@ class Player():
 
 class World():
     def __init__(self, path):
-        self.data = open(path).read().split("\n")
+        self.data = MAP_DATA
         self.width = max(len(s) for s in self.data)
         self.height = len(self.data)
         self.loads = []
