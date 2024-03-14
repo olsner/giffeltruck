5d445d1a3a415aebeebcf1409788f6883a791149
Simon Brenner <olsner@gmail.com>
2024-03-14 11:19:39 +0100

    Add new mottos, and include UTF8 support

--- encode.py
+++ encode.py
@@ -4,7 +4,12 @@ import sys
 import zlib
 
 data = sys.stdin.buffer.read()
+# Strip final newline to avoid empty line at end
+# NB: screen "layouts" might depend on the extra newline at the end of the
+# home screen and level data, double-check when updating data next time.
+if data[-1] == 10:
+    data = data[:-1]
 print(repr(data))
-print(repr(data.decode('ascii').split('\n')))
+print(repr(data.decode('utf8').split('\n')))
 data = zlib.compress(data, level=9)
 print(repr(data))
